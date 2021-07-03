50a103cfabbde63855d782f836baed900a4daf15
Zaeem khan <khanzaeem010@gmail.com>
2021-07-03 15:00:16 +0500

    Add project files.

---
+++ PCS/Controllers/MainController.cs
@@ -0,0 +1,39 @@
+﻿using PCS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PCS.Controllers
+{
+    public class MainController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public MainController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: Main
+        public ActionResult Index()
+        {
+            var List = _context.NationalFaculty.ToList();
+            var List1 = _context.InNationalFaculty.ToList();
+            ViewBag.Data = List1;
+            return View(List);
+        }
+        public ActionResult PSIC()
+        {
+            return View();
+        }
+        public ActionResult PCS()
+        {
+            return View();
+        }
+    }
+}
---
+++ PCS/Models/NationalFaculty.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PCS.Models
+{
+    public class NationalFaculty
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public string Name { get; set; }
+    }
+    public class InNationalFaculty
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public string Name { get; set; }
+    }
+}
